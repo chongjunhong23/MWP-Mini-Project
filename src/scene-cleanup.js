bbb0708c4eec5faa06dd6fceb41b2f6013632a7f
Chong Jun Hong <148421606+chongjunhong23@users.noreply.github.com>
2026-07-10 16:48:41 +0800

    Seal lab door wall gap

--- src/scene-cleanup.js
+++ src/scene-cleanup.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three';
 
 const hiddenSceneObjectNames = new Set([
-  'Lab Door Exterior Header Fill',
-  'Lab Door Lower Jamb',
-  'Lab Door Upper Jamb'
+  'Lab Door Exterior Header Fill'
 ]);
 
 const flattenedDoorFrameGeometry = new Map([
-  ['Lab Door Top Lintel', [0.22, 0.72, 2.05]]
+  ['Lab Door Lower Jamb', [0.035, 2.45, 0.32]],
+  ['Lab Door Upper Jamb', [0.035, 2.45, 0.24]],
+  ['Lab Door Top Lintel', [0.035, 0.72, 2.05]]
 ]);
 
 function flattenDoorFrameObject(object) {
@@ -18,6 +18,7 @@ function flattenDoorFrameObject(object) {
   }
 
   object.geometry = new THREE.BoxGeometry(...geometrySize);
+  object.position.x = -9.795;
 }
 
 if (!THREE.Object3D.prototype.__fcN28SceneCleanupPatched) {
